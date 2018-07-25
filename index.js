74c1ca77b129ae0f4217a1ff82673302eed0442a
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-07-25 18:46:23 +0000

    broker update

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-
+var BlinkTradeRest = require("blinktrade").BlinkTradeRest;
+var blinktrade = new BlinkTradeRest({ currency: "BRL", brokerId: 11 });
 
 
 var BlinkTradeWS = require("blinktrade").BlinkTradeWS;
-var blinktradeSocket = new BlinkTradeWS({ prod: true });
+var blinktradeSocket = new BlinkTradeWS({ prod: true, brokerId: 11 });
 
+blinktrade.orderbook().then(function(orderbook) {
+  console.log(orderbook);
+});
 
 
 function login(name,passwd,broker,res){
@@ -23,8 +27,7 @@ function login(name,passwd,broker,res){
   }
 
 
-function getDepositAddress(name, passwd, broker, res) {
-    
+function getDepositAddress(name,passwd,broker,res) {
     blinktradeSocket.connect().then(function() {
       return blinktradeSocket.login({ username: name, password: passwd, brokerId: broker }).catch(e => {
         res.json({'error': 'wrong password', 'exception': e});
